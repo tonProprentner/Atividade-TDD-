03889452bbcc2f360f52fd305f60b835f0d3c6ad
tonProprentner <124644432+tonProprentner@users.noreply.github.com>
2023-02-07 23:13:29 -0300

    Atividade Quinta TDD

--- Polindromo.js
+++ Polindromo.js
@@ -9,8 +9,6 @@ Scenario: Verify if a word is not a palindrome
   Given a word "A car"
   When I check if it is a palindrome
   Then the result should be false
-const { Given, When, Then } = require('cucumber');
-const assert = require('assert');
 
 function isPalindrome(word) {
   word = word.toLowerCase().replace(/[^a-z0-9]/g, '');
